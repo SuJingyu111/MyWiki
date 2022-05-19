72febb0e4ae037a68bde8023454b9bf373a97590
jingyusu <gar123su@gmail.com>
2022-05-19 00:46:25 -0400

    added test for hello interface: test.http

--- src/main/java/com/JYS/wiki/controller/TestController.java
+++ src/main/java/com/JYS/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.JYS.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +12,8 @@ public class TestController {
         return "hello world";
     }
 
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "hello world! Post: " + name;
+    }
 }
