bba89daf37e30c433be1c19204e49d4146cf48bf
jingyusu <gar123su@gmail.com>
2022-05-20 02:14:33 -0400

    query demo table

---
+++ src/main/java/com/JYS/wiki/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.JYS.wiki.service;
+
+import com.JYS.wiki.domain.Demo;
+import com.JYS.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list() {
+        return demoMapper.selectByExample(null);
+    }
+}
