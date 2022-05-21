5880d20d416cf64ac0bec63b92d997a09437eb6b
jingyusu <gar123su@gmail.com>
2022-05-21 19:31:17 -0400

    list query interface support dynamic SQL cover icon adjustment

--- src/main/java/com/JYS/wiki/service/EbookService.java
+++ src/main/java/com/JYS/wiki/service/EbookService.java
@@ -7,6 +7,7 @@ import com.JYS.wiki.req.EbookReq;
 import com.JYS.wiki.resp.EbookResp;
 import com.JYS.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -20,7 +21,9 @@ public class EbookService {
     public List<EbookResp> list(EbookReq req) {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%" + req.getName() + "%");
+        if (!ObjectUtils.isEmpty(req.getName())) {
+            criteria.andNameLike("%" + req.getName() + "%");
+        }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
         List<EbookResp> respList = CopyUtil.copyList(ebookList, EbookResp.class);
         return respList;
