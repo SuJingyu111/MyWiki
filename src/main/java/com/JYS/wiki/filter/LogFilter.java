3344b03eb78fb4bc2390f5c97f151dbb76b74337
jingyusu <gar123su@gmail.com>
2022-05-21 20:15:42 -0400

    added filter to print time

---
+++ src/main/java/com/JYS/wiki/filter/LogFilter.java
@@ -0,0 +1,33 @@
+package com.JYS.wiki.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class LogFilter implements Filter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        // Print req info
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        LOG.info("------------- LogFilter Start -------------");
+        LOG.info("Request Address: {} {}", request.getRequestURL().toString(), request.getMethod());
+        LOG.info("Remote Address: {}", request.getRemoteAddr());
+
+        long startTime = System.currentTimeMillis();
+        filterChain.doFilter(servletRequest, servletResponse);
+        LOG.info("------------- LogFilter End Time Elapsed：{} ms -------------", System.currentTimeMillis() - startTime);
+    }
+}
