2a1da930ad716a58bb66f672ccd623af1651a083
jingyusu <gar123su@gmail.com>
2022-05-21 20:06:38 -0400

    axios interceptor, print req args and return result/err

--- web/src/main.ts
+++ web/src/main.ts
@@ -9,6 +9,24 @@ import axios from "axios";
 
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 
+/**
+ * axios interceptors
+ */
+axios.interceptors.request.use(function (config) {
+    console.log('req args：', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('return result：', response);
+    return response;
+}, error => {
+    console.log('return err：', error);
+    return Promise.reject(error);
+});
+
+
 const app = createApp(App).use(store).use(router).use(Antd);
 app.mount('#app');
 
